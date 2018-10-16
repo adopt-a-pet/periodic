2cf684ec65d216c4f61c2d60d9b510bee7ad8ab6
Kevin McTigue <kevin@adoptapet.com>
2018-10-16 18:19:11 -0400

    HB-2889 Somewhat working buttons

--- src/elements/Button.vue
+++ src/elements/Button.vue
@@ -3,7 +3,7 @@
     :is="type"
     :href="href"
     :type="submit"
-    :class="['button', size, state, variation]">
+    :class="['button', `button--${color}`, `button--${size}`, state, variation]">
       <slot/>
   </component>
 </template>
@@ -35,9 +35,9 @@ export default {
      */
     size: {
       type: String,
-      default: "medium",
+      default: "large",
       validator: value => {
-        return value.match(/(small|medium|large)/)
+        return value.match(/(small|large)/)
       },
     },
     /**
@@ -79,99 +79,185 @@ export default {
         return value.match(/(primary|secondary)/)
       },
     },
+    /**
+     * Color allows us to easily change the color.
+     * `color`
+     */
+    color: {
+      type: String,
+      default: "green",
+      validator: value => {
+        return value.match(/(green)/)
+      },
+    },
   },
 }
 </script>
 
 <style lang="scss" scoped>
-.button {
-  @include reset;
-  @include stack-space($space-m);
-  @include inline-space($space-xs);
-  will-change: transform;
-  transition: all 0.2s ease;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-weight: $weight-semi-bold;
-  font-size: $size-m;
-  font-family: $font-text;
-  line-height: $line-height-m;
-  text-decoration: none;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  border: 0;
-  box-shadow: inset 0 0 0 2px $color-bleu-de-france;
-  border-radius: $radius-default;
-  background: transparent;
-  color: $color-bleu-de-france;
-  cursor: pointer;
-  &:hover,
-  &.hover {
-    color: $color-white;
-    background: $color-bleu-de-france;
-    transform: translateZ(0) scale(1.03);
-  }
-  &:active,
-  &.active {
-    transition: none;
-    background: $color-bleu-de-france-dark;
-    box-shadow: none;
-    color: $color-white;
-    transform: translateZ(0) scale(1);
-  }
+$btn-shade-hover: inset 0 -3px 0 0 rgba(0, 0, 0, 0.4);
+$btn-shade-active: inset 0 3px 4px 0 rgba(0, 0, 0, 0.4);
 
-  &:focus,
-  &.focus {
-    background: $color-bleu-de-france-darker;
-    box-shadow: none;
-    color: $color-white;
-    transform: translateZ(0) scale(1);
+.button {
+  display: block;
+  background-color: color(awhite, base);
+  @include border(2px solid color(agray, light));
+  border-radius: $border-radius;
+  color: color(agray, light);
+  // font-family: $museo700;
+  font-weight: 700;
+  @include rem-fallback("font-size", 20px);
+  cursor: pointer;
+  line-height: 2.7;
+  width: 100%;
+  transition: background 0.2s ease;
+  margin: 0 auto;
+  padding: 0;
+
+  &:focus {
     outline: 0;
   }
 
-  // For icons inside buttons
-  .icon {
-    float: right;
-    margin: -#{$space-xs} -#{$space-xs} -#{$space-s} $space-xs/2;
-    color: $color-bleu-de-france;
+  &:hover {
+    background-color: $color_forty_per_lighter_grey;
+    @include border(2px solid color(agray, light));
+    color: color(agray, light);
   }
 
-  // Various button sizes
-  &.large {
-    @include inset-squish-space($space-s);
-    font-size: $size-l;
-  }
-  &.medium {
-    @include inset-squish-space($space-s);
-    font-size: $size-m;
-  }
-  &.small {
-    @include inset-squish-space($space-xs);
-    font-size: $size-s;
+  &:active {
+    background-color: color(agray, light);
+    color: color(awhite, base);
   }
 
-  // Primary button
-  &.primary {
-    background: $color-bleu-de-france;
-    color: $color-white;
-    box-shadow: none;
-    &:hover,
-    &.hover {
-      background-color: shade($color-bleu-de-france, 12%);
+  @include modifier("disabled") {
+    background-color: color(agray, lighter);
+    border: 0;
+    line-height: 2.9;
+    cursor: not-allowed;
+    pointer-events: none;
+    color: color(awhite);
+  }
+
+  @include modifier("small") {
+    line-height: 2.5;
+    @include rem-fallback("font-size", 16px);
+  }
+
+  @include modifier("small-disabled") {
+    border: 0;
+    line-height: 2.2;
+    background-color: color(agray, lighter);
+    cursor: not-allowed;
+    pointer-events: none;
+    color: color(awhite);
+  }
+
+  @include modifier("green") {
+    background-color: color(agreen, base);
+    border: 0;
+    line-height: 2.9;
+    color: color(awhite, base);
+
+    &:hover {
+      background-color: color(agreen, dark);
+      border: 0;
+      box-shadow: $btn-shade-hover;
+      color: color(awhite, base);
     }
-    &:active,
-    &.active {
-      background-color: shade($color-bleu-de-france, 20%);
-      transition: none;
-    }
-    &:focus {
-      outline: 0;
-    }
-    .user-is-tabbing &:focus,
-    &.focus {
+
+    &:active {
+      background-color: color(agreen, base);
+      box-shadow: $btn-shade-active;
     }
   }
+
+  @include modifier("green-small") {
+    @extend .button--green;
+    @extend .button--small;
+    line-height: 2.75;
+  }
+
+  @include modifier("orange") {
+    background-color: color(aorange, base);
+    border: 0;
+    line-height: 2.9;
+    color: color(awhite, base);
+
+    &:hover {
+      background-color: color(aorange, dark);
+      border: 0;
+      box-shadow: $btn-shade-hover;
+      color: color(awhite, base);
+    }
+
+    &:active {
+      background-color: color(aorange, base);
+      box-shadow: $btn-shade-active;
+    }
+  }
+
+  @include modifier("orange-small") {
+    @extend .button--orange;
+    @extend .button--small;
+    line-height: 2.75;
+  }
+
+  @include modifier("blue") {
+    background-color: color(ablue, base);
+    border: 0;
+    line-height: 2.9;
+    color: color(awhite, base);
+
+    &:hover {
+      box-shadow: $btn-shade-hover;
+      border: 0;
+      background-color: color(ablue, dark);
+      color: color(awhite, base);
+    }
+
+    &:active {
+      box-shadow: $btn-shade-active;
+      background-color: color(ablue, base);
+    }
+  }
+
+  @include modifier("blue-small") {
+    @extend .button--blue;
+    @extend .button--small;
+    line-height: 2.75;
+  }
+
+  &.is-disabled {
+    border: 0;
+    background-color: color(agray, lighter);
+    cursor: not-allowed;
+    pointer-events: none;
+    color: color(awhite);
+  }
+
+  @include element("image") {
+    float: left;
+    @include rem-fallback("width", 58px);
+    @include rem-fallback("height", 58px);
+    display: flex;
+  }
+
+  @include element("icon") {
+    fill: color(awhite);
+    @include rem-fallback("width", 24px);
+    @include rem-fallback("height", 23px);
+    display: inline-flex;
+    vertical-align: middle;
+    @include rem-fallback("margin-right", 5px);
+  }
+
+  // @include element ('ask-about-me') {
+  //     font-family: $museo700;
+  //     color: color(awhite);
+  //     @include font-size(20);
+  //     @include rem-fallback('line-height', 26px);
+  //     @include rem-fallback('letter-spacing', -.25px);
+  // }
 }
 </style>
 
@@ -179,12 +265,17 @@ export default {
   ```jsx
   <div>
     <Button variation="primary" size="large">Primary Button</Button>
+    <br />
     <Button variation="primary" size="medium">Medium</Button>
+    <br />
     <Button variation="primary" size="small">Small</Button>
     <br />
     <Button>Default Button</Button>
+    <br />
     <Button state="hover">:hover</Button>
+    <br />
     <Button state="active">:active</Button>
+    <br />
     <Button state="focus">:focus</Button>
   </div>
   ```
