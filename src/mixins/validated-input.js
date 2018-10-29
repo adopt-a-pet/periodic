0447675956a14ce767604e31a00650b8d291e4bb
Kevin McTigue <kevin@adoptapet.com>
2018-10-29 12:05:03 -0400

    HB-2898 Showing password

---
+++ src/mixins/validated-input.js
@@ -0,0 +1,44 @@
+import { mixin as veeValidateMixin, directive as veeValidateDirective } from "vee-validate"
+
+export default {
+  mixins: [veeValidateMixin],
+  directives: { validate: veeValidateDirective },
+
+  methods: {
+    onInput(value) {
+      this.$emit("change", value)
+    },
+    onFocus(value) {
+      this.$emit("focus", value)
+    },
+    setErrorMessages() {
+      if (!this.name) throw new Error("This mixin can only be used by components that have a name prop")
+      if (!this.$options.errorMessages)
+        throw new Error("This mixin can only be used by components that have errorMessages in the component options")
+
+      let messages = {}
+
+      messages[this.name] = this.$options.errorMessages
+
+      this.$validator.localize("en", {
+        custom: messages,
+      })
+    },
+  },
+
+  computed: {
+    errorState() {
+      return this.errors.has(this.name)
+    },
+    errorMessage() {
+      return this.errors.first(this.name)
+    },
+    successState() {
+      return this.password && !this.errorState
+    },
+  },
+
+  created() {
+    this.setErrorMessages()
+  },
+}
