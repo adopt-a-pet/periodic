262671cef55d09a4da2afb5d38869cef9431e8d8
Stephen Gashler <stephengashler@Stephens-MacBook-Pro.local>
2019-05-28 14:00:42 -0600

    Style fix for button and email verification message for NPASignupForm

--- src/patterns/NPASignupForm.vue
+++ src/patterns/NPASignupForm.vue
@@ -171,7 +171,8 @@ export default {
   computed: {
     emailConfirmValidators() {
       return {
-        emailConfirm: value => value.toLowerCase() === this.form.email.toLowerCase(),
+        emailConfirm: value =>
+          value.toLowerCase() === this.form.email.toLowerCase(),
       };
     },
   },
@@ -236,7 +237,7 @@ export default {
 <docs>
 ```vue
 <template>
-  <NPASignupForm :offers="offers" />
+  <NPASignupForm :offers="offers"/>
 </template>
 <script>
 export default {
