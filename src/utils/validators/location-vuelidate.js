88f110c9aa1aa1bcd2236549d1d96274398d0311
Tim Murphy <tim@adoptapet.com>
2019-09-17 17:18:06 -0400

    HB-4533: remove redundant validators

--- src/utils/validators/location-vuelidate.js
+++ src/utils/validators/location-vuelidate.js
@@ -1,15 +1,3 @@
-import { helpers } from 'vuelidate/lib/validators';
-
-export const zipValidator = helpers.regex(
-  'zip',
-  /^[0-9]{5}(?:-[0-9]{4})?$/,
-);
-
-export const canadianZipValidator = helpers.regex(
-  'canadianZip',
-  /^([a-zA-Z0-9]{3}((\s)?[a-zA-Z0-9]{3})?$)$/,
-);
-
 export const zipsValidator = value => {
   const zipCode = /^[0-9]{5}(?:-[0-9]{4})?$/;
   const canadianZip = /^([a-zA-Z0-9]{3}((\s)?[a-zA-Z0-9]{3})?$)$/;
