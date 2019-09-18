d86008768770cf2419af6b4d7f39d37c59a25aba
Stephen Gashler <steve@adoptapet.com>
2019-09-18 15:38:03 -0600

    New sycalls and searchForm pattern

--- docs/docs.syscalls.js
+++ docs/docs.syscalls.js
@@ -5,19 +5,46 @@ export default {
     'api/pets/getBreeds': function (_, payload) {
       return Promise.resolve([
         {
+          clanId: 1,
           breedId: 187,
           breedName: 'Affenpinscher',
           breedNamePlural: 'Affenpinschers',
         },
         {
+          clanId: 1,
           breedId: 1,
           breedName: 'Afghan Hound',
           breedNamePlural: 'Afghan Hounds',
         },
         {
+          clanId: 1,
           breedId: 2,
           breedName: 'Airedale Terrier',
           breedNamePlural: 'Airedale Terriers',
+        },
+      ]);
+    },
+    'api/pets/getClans': function (_, payload) {
+      return Promise.resolve([
+        {
+          clanId: 1,
+          clanName: 'dog',
+          clanNamePluralPretty: 'Dogs'
+        },
+        {
+          clanId: 2,
+          clanName: 'cat',
+          clanNamePluralPretty: 'Cats'
+        },
+        {
+          clanId: 3,
+          clanName: 'rabbit',
+          clanNamePluralPretty: 'Rabbits'
+        },
+        {
+          clanId: 4,
+          clanName: 'small animal',
+          clanNamePluralPretty: 'Small Animals'
         },
       ]);
     },
@@ -26,6 +53,84 @@ export default {
         {
           colorId: 153,
           colorName: 'Brindle',
+        },
+      ]);
+    },
+    'api/pets/getSizes': function (_, payload) {
+      return Promise.resolve([
+        {
+          clanId: 5,
+          sizeId: 12,
+          petSizeRangeName: 'Large',
+        },
+        {
+          clanId: 8,
+          sizeId: 20,
+          petSizeRangeName: 'Small',
+        },
+        {
+          clanId: 1,
+          sizeId: 1,
+          petSizeRangeName: 'Small 25 lbs or less',
+        },
+        {
+          clanId: 1,
+          sizeId: 4,
+          petSizeRangeName: 'X-Large 101 lbs or more,'
+        },
+      ]);
+    },
+    'api/pets/getColors': function (_, payload) {
+      return Promise.resolve([
+        {
+          clanId: 1,
+          colorId: 152,
+          colorName: 'Black',
+        },
+        {
+          clanId: 1,
+          colorId: 153,
+          colorName: 'Brindle',
+        },
+        {
+          clanId: 1,
+          colorId: 154,
+          colorName: 'Brown/Chocolate',
+        },
+      ]);
+    },
+    'api/pets/getAges': function (_, payload) {
+      return Promise.resolve([
+        {
+          age: 'adult',
+          clanId: 4
+        },
+        {
+          age: 'baby',
+          clanId: 4
+        },
+        {
+          age: 'puppy',
+          clanId: 1
+        },
+      ]);
+    },
+    'api/pets/getHair': function (_, payload) {
+      return Promise.resolve([
+        {
+          clanId: 2,
+          hairId: 11,
+          hairName: 'short'
+        },
+        {
+          clanId: 2,
+          hairId: 1,
+          hairName: 'medium'
+        },
+        {
+          clanId: 2,
+          hairId: 3,
+          hairName: 'long'
         },
       ]);
     },
--- src/patterns/SearchForm.vue
+++ src/patterns/SearchForm.vue
@@ -1,84 +1,255 @@
 <template>
-  <div>
+  <div :class="b().toString()">
     <Heading level="h3">
       Find a Pet to Adopt
     </Heading>
     <nav>
       <a
-        v-for="section of sections"
+        v-for="(section, index) of sections"
         :key="section"
-        @click="activeSection = section">{{ section }}</a>
+        @click="activeSectionIndex = index">{{ section }}
+        <div
+          :class="{ active: activeSectionIndex === index }"
+          class="underline" />
+      </a>
     </nav>
-    <section v-if="activeSection === 'Dogs'">
+    <section v-if="sections[activeSectionIndex] === 'Dogs'">
       <TextInput
         v-model="fields.zipcode"
         label="Zip / Postal or City, State" />
-      {{ fields.zipcode }}
+      <Dropdown
+        v-model="fields.distance"
+        :items="distances"
+        label="Distance" />
+      <Dropdown
+        v-model="fields.breedId"
+        :items="breeds"
+        :search="true"
+        :special-choices="[{ display: 'Any', value: null }]"
+        label="Breed" />
+      <Dropdown
+        v-model="fields.sex"
+        :items="sexes"
+        :special-choices="[{ display: 'Any', value: null }]"
+        label="Sex" />
+      <Dropdown
+        v-model="fields.size"
+        :items="sizes"
+        :special-choices="[{ display: 'Any', value: null }]"
+        label="Size" />
+      <Dropdown
+        v-model="fields.color"
+        :items="colors"
+        :special-choices="[{ display: 'Any', value: null }]"
+        label="Color" />
+      <Dropdown
+        v-model="fields.age"
+        :items="ages"
+        :special-choices="[{ display: 'Any', value: null }]"
+        label="Age" />
     </section>
-    <section v-if="activeSection === 'Cats'">
-      Cats
+    <section v-if="sections[activeSectionIndex] === 'Cats'">
+      <TextInput
+        v-model="fields.zipcode"
+        label="Zip / Postal or City, State" />
+      <Dropdown
+        v-model="fields.distance"
+        :items="distances"
+        label="Distance" />
+      <Dropdown
+        v-model="fields.breedId"
+        :items="breeds"
+        :search="true"
+        :special-choices="[{ display: 'Any', value: null }]"
+        label="Breed" />
+      <Dropdown
+        v-model="fields.sex"
+        :items="sexes"
+        :special-choices="[{ display: 'Any', value: null }]"
+        label="Sex" />
+      <Dropdown
+        v-model="fields.size"
+        :items="sizes"
+        :special-choices="[{ display: 'Any', value: null }]"
+        label="Size" />
+      <Dropdown
+        v-model="fields.hair"
+        :items="hairs"
+        :special-choices="[{ display: 'Any', value: '' }]"
+        label="Hair" />
+      <Dropdown
+        v-model="fields.age"
+        :items="ages"
+        :special-choices="[{ display: 'Any', value: null }]"
+        label="Age" />
     </section>
-    <section v-if="activeSection === 'Others'">
-      Others
+    <section v-if="sections[activeSectionIndex] === 'Others'">
+      <Dropdown
+        v-model="fields.clanId"
+        :items="clans"
+        :special-choices="[{ display: 'Any', value: null }]"
+        label="Others" />
     </section>
-    <Dropdown
-      v-model="fields.distance"
-      :items="distances"
-      label="Distance" />
-    <Dropdown
-      v-model="fields.search"
-      :items="distances"
-      :search="true"
-      label="Breed" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'SearchForm',
+  blockName: 'search-form',
   status: 'under-review',
   release: '1.0.0',
   data() {
-    // get breeds
-    let breeds;
-    this.$syscall('getBreeds')
-      .then(response => {
-        breeds = response;
-      });
-    return {
-      activeSection: 'Dogs',
-      breeds,
-      distances: [
-        {
-          display: '35 miles or less',
-          value: 35,
-        },
-        {
-          display: '50 miles or less',
-          value: 50,
-        },
-        {
-          display: '75 miles or less',
-          value: 75,
-        },
-        {
-          display: '100 miles or less',
-          value: 100,
-        },
-        {
-          display: '250 miles or less',
-          value: 250,
-        },
-      ],
-      fields: {
-        distance: this.distances[0].value,
+    this.getData();
+    const distances = [
+      {
+        display: '35 miles or less',
+        value: 35,
       },
+      {
+        display: '50 miles or less',
+        value: 50,
+      },
+      {
+        display: '75 miles or less',
+        value: 75,
+      },
+      {
+        display: '100 miles or less',
+        value: 100,
+      },
+      {
+        display: '250 miles or less',
+        value: 250,
+      },
+    ];
+    return {
+      activeSectionIndex: 0,
+      ages: [],
+      allAges: [],
+      allColors: [],
+      allBreeds: [],
+      allClans: [],
+      allHairs: [],
+      allSizes: [],
+      breeds: [],
+      clans: [],
+      colors: [],
+      distances,
+      fields: {
+        distance: distances[0].value,
+      },
+      hairs: [],
       sections: [
         'Dogs',
         'Cats',
         'Others',
       ],
+      sexes: [
+        {
+          display: 'Male',
+          value: 'male',
+        },
+        {
+          display: 'Female',
+          value: 'female',
+        },
+      ],
+      sizes: [],
     };
+  },
+  methods: {
+    /**
+     * Get API data
+     */
+    getData() {
+      this.$syscall('api/pets/getClans')
+        .then(response => {
+          this.allClans = response;
+          this.formatList('clan', null, 'Select Pet Type');
+
+          // create array of clan ID's
+          const clanIds = [];
+          response.forEach(clan => {
+            clanIds.push(clan.clanId);
+          });
+
+          // get breeds
+          this.$syscall('api/pets/getBreeds', clanIds)
+            .then(res => {
+              this.allBreeds = res;
+              this.filterList('breed');
+              this.formatList('breed');
+            });
+
+          // get sizes
+          this.$syscall('api/pets/getSizes', clanIds)
+            .then(res => {
+              this.allSizes = res;
+              this.filterList('size');
+              this.formatList('size', 'petSizeRangeName');
+            });
+
+          // get colors
+          this.$syscall('api/pets/getColors', clanIds)
+            .then(res => {
+              this.allColors = res;
+              this.filterList('color');
+              this.formatList('color');
+            });
+
+          // get ages
+          this.$syscall('api/pets/getAges', clanIds)
+            .then(res => {
+              this.allAges = res;
+              this.filterList('age');
+              this.formatList('age', 'age');
+            });
+
+          // get hair
+          this.$syscall('api/pets/getHair', clanIds)
+            .then(res => {
+              this.allHairs = res;
+              this.filterList('hair');
+              this.formatList('hair');
+            });
+        });
+    },
+
+    /**
+     * Filter and format lists
+     */
+    filterList(listName) {
+      if (this[`all${this.$options.filters.capitalize(listName)}s`].length) {
+        let clanId;
+        if (this.activeSection === 'Dogs') {
+          clanId = 1;
+        } else if (this.activeSection === 'Cats') {
+          clanId = 2;
+        } else if (this.clan) {
+          clanId = this.clan.clanId;
+        }
+        this[`${listName}s`] = this[`all${this.$options.filters.capitalize(listName)}s`].filter(listItem => listItem.clanId === clanId);
+      }
+    },
+
+    /**
+     * Format list
+     */
+    formatList(listName, nameKey) {
+      const nameKey2 = nameKey || `${listName}Name`;
+      let value;
+      const formattedList = [];
+
+      this[`all${this.$options.filters.capitalize(listName)}s`].forEach(listItem => {
+        value = listItem[`${listName}Id`] ? listItem[`${listName}Id`] : listItem[nameKey2];
+        formattedList.push({
+          display: this.$options.filters.capitalize(listItem[nameKey2]),
+          value,
+        });
+      });
+      this[`${listName}s`] = formattedList;
+    },
   },
 };
 </script>
