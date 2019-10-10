ab7c09261204bae541b99ed2bf5b7374e9fb2be4
Stephen Gashler <steve@adoptapet.com>
2019-10-10 15:28:52 -0600

    HB-4790: new DropdownGroup element

---
+++ src/elements/DropdownGroup.vue
@@ -0,0 +1,49 @@
+<template>
+  <div :class="b().toString()">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DropdownGroup',
+  status: 'under-review',
+  release: '1.0.0',
+  blockName: 'dropdown-group',
+};
+</script>
+
+<docs>
+  ```jsx
+  <DropdownGroup>
+    <Dropdown
+      label="Without Search"
+      v-model="dropdown1"
+      :items="[
+        { display: 'One', value: 1 },
+        { display: 'Two', value: 2 },
+      ]" />
+    <Dropdown
+      label="With Search"
+      v-model="dropdown2"
+      :items="[
+        { display: 'One', value: 1 },
+        { display: 'Two', value: 2 },
+      ]"
+      :search="true"
+      :specialChoices="[{ display: 'Any', value: null }]"
+      tooltip="This is an info bubble" />
+    <DropdownMulti
+      label="With Search"
+      v-model="dropdown2"
+      :items="[
+        { display: 'One', value: 1, labelRight: '(0)' },
+        { display: 'Two', value: 2, labelRight: '(2)' },
+        { display: 'Three', value: 3, labelRight: '(5)' },
+        { display: 'Four', value: 4, labelRight: '(1)' },
+      ]"
+      :search="true"
+      tooltip="This is an info bubble" />
+  </DropdownGroup>
+  ```
+</docs>
