59570167f7821f5c0594a3a6316003d860019ab4
Stephen Gashler <steve@adoptapet.com>
2019-10-18 12:57:18 -0600

    New popup element

---
+++ src/elements/Popup.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :class="b().toString()">
+    <div :class="b('bubble-container').toString()">
+      <div :class="b('bubble').toString()">
+        <div :class="b('content').toString()">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Popup',
+  blockName: 'popup',
+  status: 'under-review',
+  release: '1.0.0',
+  model: {
+    event: 'change',
+  },
+  props: {
+    /**
+     * The icon to use for the popup.
+     */
+    icon: {
+      type: String,
+      default: 'info',
+    },
+  },
+  data() {
+    return {
+      showBubble: false,
+    };
+  },
+};
+</script>
+
+<docs>
+  ```jsx
+  <Popup>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.
+  </Popup>
+  ```
+</docs>
