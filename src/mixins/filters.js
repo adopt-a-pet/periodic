3f4567e81aef96ec971734421cace351b099fed5
Stephen Gashler <steve@adoptapet.com>
2019-11-26 14:20:14 -0700

    HB-4949: fix for ordering ages

--- src/mixins/filters.js
+++ src/mixins/filters.js
@@ -3,11 +3,31 @@
 
 export default {
   filters: {
-  // Function to Capitalize the value
+    /**
+     * Capitalize the value
+     */
     capitalize(value) {
       if (!value) return '';
       const stringValue = value.toString();
       return stringValue.charAt(0).toUpperCase() + stringValue.slice(1);
+    },
+
+    /**
+     * Order an array of age objects
+     */
+    orderAges(ages) {
+      const agesOrdered = [];
+      const order = [
+        'Any', 'Baby', 'Puppy', 'Kitten', 'Young', 'Adult', 'Senior',
+      ];
+      order.forEach(key => {
+        ages.forEach(age => {
+          if (age.display === key) {
+            agesOrdered.push(age);
+          }
+        });
+      });
+      return agesOrdered;
     },
   },
 };
