26c8c910423a99c584402f8d13386f214ca28612
Becky McKimmy <becky@adoptapet.com>
2019-11-27 12:13:19 -0500

    HB-4874: Move search query sentence to separate pattern

--- src/patterns/NPASignupForm.vue
+++ src/patterns/NPASignupForm.vue
@@ -40,26 +40,10 @@
 
       <VSpacer size="l" />
 
-      <div v-if="filters.clan === 1 || filters.clan === 2">
-        <Paragraph
-          font-size="s"
-          font-weight="bold"
-          line-height="24px">
-          You’re Searching For
-        </Paragraph>
+      <SearchQuerySentence
+        :filters="filters" />
 
-        <Paragraph
-          font-weight="bold"
-          line-height="26px">
-          <TextLink
-            :class="b('search-params').toString()"
-            @click="searchFilters">
-            {{ clanName }}s: {{ petDescription }} - within {{ geoRange }} miles of {{ filters.zipCode }}
-          </TextLink>
-        </Paragraph>
-
-        <VSpacer size="l" />
-      </div>
+      <VSpacer size="l" />
 
       <div :class="b('fields').toString()">
         <EmailInput
@@ -303,8 +287,6 @@ export default {
         plan: this.plan,
       },
       paymentInfo: {},
-      colorsMap: {},
-      breedMap: {},
     };
   },
   blockName: 'npa-signup',
@@ -312,122 +294,6 @@ export default {
   release: '1.0.0',
 
   computed: {
-    sizeMap() {
-      return {
-        1: 'Small',
-        2: 'Medium',
-        3: 'Large',
-        4: 'X-Large',
-      };
-    },
-
-    sexFullName() {
-      if (!this.filters.sex) return '';
-
-      let name = '';
-
-      switch (this.filters.sex.join()) {
-        case '':
-          name = '';
-          break;
-
-        case 'm':
-          name = 'Male';
-          break;
-
-        case 'f':
-          name = 'Female';
-          break;
-
-        default:
-          name = 'Male or Female';
-      }
-
-      return name;
-    },
-    age() {
-      return this.filters.age ? this.filters.age.map(this.$options.filters.capitalize).join(' or ') : null;
-    },
-    hairNames() {
-      return this.filters.hair ? this.filters.hair.map(this.$options.filters.capitalize).join(' or ') : null;
-    },
-    colorNames() {
-      if (!this.filters.color) return '';
-
-      return this.filters.color
-        .map(colorId => this.colorsMap[colorId])
-        .join(' or ');
-    },
-    familyNames() {
-      if (this.filters.breeds) {
-        return this.filters.breeds
-          .map(breedId => this.breedMap[breedId])
-          .sort()
-          .join(' or ');
-      }
-      return '';
-    },
-    hasMoreFiltersThanClan() {
-      return Boolean(
-        this.age || this.sexFullName || this.colorNames || this.familyNames || this.sizeNames || this.hairNames,
-      );
-    },
-
-    clanName() {
-      let name;
-
-      switch (this.filters.clan) {
-        case 1:
-          name = 'dog';
-          break;
-
-        case 2:
-          name = 'cat';
-          break;
-
-        default:
-          name = 'pet';
-      }
-
-      return name;
-    },
-
-    bondedPair() {
-      return this.filters.bondedPair ? 'bonded pair' : '';
-    },
-
-    specialNeeds() {
-      return this.filters.specialNeeds ? 'with special needs' : '';
-    },
-
-    sizeNames() {
-      if (!(this.filters.size && this.filters.size.length)) return '';
-
-      return this.filters.size
-        .map(sizeId => this.sizeMap[sizeId])
-        .join(' or ');
-    },
-
-    geoRange() {
-      return this.filters.geoRange === -1 ? 3500 : this.filters.geoRange;
-    },
-
-    petDescription() {
-      if (this.hasMoreFiltersThanClan) {
-        return [
-          this.familyNames,
-          this.sexFullName,
-          this.age,
-          this.colorNames,
-          this.sizeNames,
-          this.hairNames,
-        ]
-          .filter(a => !!a)
-          .join(', ');
-      }
-      return 'All';
-    },
-
     npaTypes: () => [
       {
         heading: 'Premium Alert',
@@ -444,38 +310,6 @@ export default {
         value: 0,
       },
     ],
-  },
-
-  created() {
-    /**
-     * Get colors name and Ids from database
-     *
-     * @syscall api/pets/colors
-     * @param {Number}
-     * @returns {{colorId: Number, colorName: String}}
-     */
-    this.$syscall('api/pets/getColors', this.filters.clan)
-      .then(response => {
-        this.colorsMap = response.reduce(
-          (acc, { colorId, colorName }) => Object.assign(acc, { [colorId]: colorName }),
-          {},
-        );
-      });
-
-    /**
-     * Get Breed name and Ids from database
-     *
-     * @syscall api/pets/getBreeds
-     * @param {Number}
-     * @returns {{breedId: Number, breedName: String}}
-     */
-    this.$syscall('api/pets/getBreeds', this.filters.clan).then(response => {
-      this.breedMap = response.reduce(
-        (acc, { breedId, breedName }) =>
-          Object.assign(acc, { [breedId]: breedName }),
-        {},
-      );
-    });
   },
 
   methods: {
