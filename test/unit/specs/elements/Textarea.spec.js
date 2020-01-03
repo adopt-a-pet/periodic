9db92a7ce084df76a0c7e6eb6e58c254cb023804
Stephen Gashler <steve@adoptapet.com>
2020-01-03 07:36:46 -0700

    HB-5128: updated TextArea unit test for troubleshooting

--- test/unit/specs/elements/Textarea.spec.js
+++ test/unit/specs/elements/Textarea.spec.js
@@ -5,20 +5,47 @@ const Vue = createLocalVue();
 
 Vue.use(Periodic);
 
+const errorMessages = {
+  required: 'Come on! Put something here!',
+  min: 'Needs to be longer than 10 characters',
+};
 const TestComponent = Vue.extend({
-  template: '<Textarea @blur="$emit(\'blur\')" @input="$emit(\'input\')"></Textarea>',
+  template: `<Textarea
+      v-model="textarea"
+      :min="10"
+      :errorMessages="{
+        required: '${errorMessages.required}',
+        min: '${errorMessages.min}'
+      }"
+      placeholder="This textarea is required and must have more than 10 characters"
+      shortLabel="Required and at least 10 characters"
+      required
+      @blur="$emit('blur')"
+      @input="$emit('input')" />`,
 });
 
 describe('elements/Textarea', () => {
-  const wrapper = mount(TestComponent);
-
-  it('textarea input', () => {
-    wrapper.find('textarea').setValue('Some text');
-    expect(wrapper.emitted('input').length).toBe(1);
+  const wrapper = mount(TestComponent, {
+    data() {
+      return {
+        textarea: '',
+      };
+    },
   });
+  const textarea = wrapper.find('textarea');
 
-  it('textarea blur', () => {
-    wrapper.find('textarea').trigger('blur');
+  it('emits blur event and shows error message for empty required field', () => {
+    textarea.setValue('foo');
+    textarea.setValue('');
+    textarea.trigger('blur');
     expect(wrapper.emitted('blur').length).toBe(1);
+    expect(wrapper.text()).toContain(errorMessages.required);
   });
+
+  // it('emits input event and doesn\'t show error message for completed required field', () => {
+  //   textarea.setValue('Some text');
+  //   textarea.trigger('blur');
+  //   expect(wrapper.emitted('input').length);
+  //   expect(wrapper.text()).not.toContain(errorMessage);
+  // });
 });
